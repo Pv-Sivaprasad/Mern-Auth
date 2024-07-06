0b0ec5d43704de307b831b8a0906fde86bd39932
Sivaprasad pv <sivaprasadpv777@gmail.com>
2024-07-06 12:32:38 +0530

    connected db

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('connected to mongodb');
+}).catch((err)=>{
+    console.log('some error during connecting to mongodb',err);
+})
 
 const app= express()
 const port=7000;
-
 
 
 
